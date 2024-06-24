472950b45226203d96c93a94049ec9f4b438178b
kikeprime <hermandavidtamas@gmail.com>
2024-06-24 13:22:36 +0200

    Add Powerups_HyperSonic as a define

--- SA2-Super-Amy/util.h
+++ SA2-Super-Amy/util.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define Powerups_HyperSonic Powerups_Unknown1
+
 void njCnkMotion(NJS_OBJECT* obj, NJS_MOTION* mot, float frame);
 void DoNextAction_r(int playerNum, char action, int unknown);
 bool __cdecl IsIngame();
